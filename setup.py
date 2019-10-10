c43bb386284a7eb329948f9e1f12ba43cf7ccc39
Adrian Gortzak <adrian.gortzak@valueguard.se>
2019-10-10 14:17:55 +0200

    Valueguard Python Client v1

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["requests>=2"]
+
+setup(
+    name="valueguard",
+    version="0.1.1",
+    author="Valueguard",
+    author_email="info@valueguard.se",
+    description="The official client for the Valueguard API",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Valueguard-Index-Sweden/valueguard-python-client",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: MIT",
+    ],
+)
---
+++ valueguard/valueguard.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import urllib.parse
+
+class Client:
+    # Static
+    #server_url = "http://10.10.2.222:31212"
+    server_url = "http://localhost:8080"
+    __oauth2_client_username = "api"
+
+    # User settings;
+    access_token = ""
+    refresh_token = ""
+
+    def __init__(self):
+        pass
+
+    # TODO url-encode username and password
+    def authenticate(self, username, password):
+        url = self.server_url + "/oauth/token?client_id=" + self.__oauth2_client_username + "&grant_type=password" \
+                                                                                            "&username=" + username + \
+              "&password=" + password
+        # print(url)
+        response = requests.post(url)
+        if response.status_code == 200:
+            response_json = json.loads(response.content.decode("utf-8"))
+            self.access_token = response_json['access_token']
+            self.refresh_token = response_json['refresh_token']
+            #print(response_json)
+        elif response.status_code != 200:
+            #print(response.status_code)
+            print(response.content.decode("utf-8"))
+            raise Exception('Invalid response from server')
+
+    def renew_access_token(self):
+        url = self.server_url + "/oauth/token?client_id=api&grant_type=refresh_token&refresh_token=" + self.refresh_token
+        print(url)
+        response = requests.post(url)
+        if response.status_code == 200:
+            response_json = json.loads(response.content.decode("utf-8"))
+            self.access_token = response_json['access_token']
+            self.refresh_token = response_json['refresh_token']
+            print(response_json)
+        else:
+            print(response.content.decode("utf-8"))
+
+    def residential_registry(self, offset, limit):
+        return self.residential_registry(offset, limit, {})
+
+    def residential_registry(self, offset, limit, search_criteria):
+        url = self.server_url + "/api/v1/residential/registry?access_token=" + self.access_token + "&offset=" + str(
+             offset) + "&limit=" + str(limit)
+        for key, value in search_criteria.items():
+            url += "&"+urllib.parse.quote(key)+"="+urllib.parse.quote(str(value))
+        #print(url)
+        session = requests.Session()
+        response = session.get(url)
+        if (response.status_code != 200):
+            print(response.content.decode("utf-8"))
+            raise Exception('Invalid response from server')
+
+        return json.loads(response.content.decode("utf-8"))
