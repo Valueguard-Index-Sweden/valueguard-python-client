1f0c17c40a83898b50727012c066419ca88c2658
Adrian Gortzak <adrian.gortzak@valueguard.se>
2020-10-02 14:48:43 +0200

    lägg till valueguard-api client på PyPi

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ requirements = ["requests>=2"]
 
 setup(
     name="valueguard",
-    version="0.1.1",
+    version="0.1.2",
     author="Valueguard",
     author_email="info@valueguard.se",
     description="The official client for the Valueguard API",
     long_description=readme,
     long_description_content_type="text/markdown",
+    license="MIT",
     url="https://github.com/Valueguard-Index-Sweden/valueguard-python-client",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: MIT",
+        "Programming Language :: Python :: 3.7"
     ],
 )
