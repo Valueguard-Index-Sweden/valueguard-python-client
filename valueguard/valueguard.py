4546bb13038fbfa74644e8c0cc785f18f834a683
Adrian Gortzak <adrian.gortzak@valueguard.se>
2021-01-19 21:22:37 +0100

    Added support for the new valuation end-point

--- valueguard/valueguard.py
+++ valueguard/valueguard.py
@@ -279,3 +279,29 @@ class Client:
               # print(response.content.decode("utf-8"))
               raise Exception(response.content.decode("utf-8"))
           return json.loads(response.content.decode("utf-8"))
+
+    def valuation(self, search_criteria=None):
+        """ Handles the query to make valuations.
+
+        Parameters
+        ----------
+        :param search_criteria:
+            Defines the search criteria used to filter the query.
+
+        Returns
+        -------
+        :return:
+            The query result in JSON format
+        """
+        if search_criteria is None:
+            search_criteria = {}
+        url = self.server_url + "/v1/valuation?access_token=" + \
+              urllib.parse.quote(self.access_token)
+        url += _generate_request_search_criteria(search_criteria.items())
+        print(url)
+        session = requests.Session()
+        response = session.get(url)
+        if response.status_code != 200:
+            # print(response.content.decode("utf-8"))
+            raise Exception(response.content.decode("utf-8"))
+        return json.loads(response.content.decode("utf-8"))
