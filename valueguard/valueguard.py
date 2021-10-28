1e5b038673b8b0764e0b20fe71cc4220f02e27ee
Adrian Gortzak <adrian.gortzak@valueguard.se>
2021-10-28 09:38:52 +0200

    Update valueguard.py

--- valueguard/valueguard.py
+++ valueguard/valueguard.py
@@ -577,3 +577,34 @@ class Client:
             # print(response.content.decode("utf-8"))
             raise Exception(response.content.decode("utf-8"))
         return json.loads(response.content.decode("utf-8"))
+    
+    def index_statistics(self, search_criteria=None, public=False):
+        """ Handles the query to get the index publishing calendar.
+
+        Parameters
+        ----------
+        :param search_criteria:
+            Defines the search criteria used to filter the query.
+
+        Returns
+        -------
+        :return:
+            The query result in JSON format
+        """
+        if search_criteria is None:
+            search_criteria = {}
+            
+        if public:
+            url = self.server_url + "/v1/index/statistics?access_token=" + \
+                urllib.parse.quote(self.access_token)
+        else: 
+            url = self.server_url + "/v1/index/statistics/public"
+            
+        url += _generate_request_search_criteria(search_criteria.items())
+        # print(url)
+        session = requests.Session()
+        response = session.get(url)
+        if response.status_code != 200:
+            # print(response.content.decode("utf-8"))
+            raise Exception(response.content.decode("utf-8"))
+        return json.loads(response.content.decode("utf-8"))
