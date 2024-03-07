2fd82c6d90d89d3e8d1229fe4beda4532432c344
robert <robert.ljungberg@valueguard.se>
2024-03-07 16:42:14 +0100

    add area information dates

--- valueguard/valueguard.py
+++ valueguard/valueguard.py
@@ -872,3 +872,31 @@ class Client:
             # print(response.content.decode("utf-8"))
             raise Exception(response.content.decode("utf-8"))
         return json.loads(response.content.decode("utf-8"))
+
+    def area_information_dates(self, search_criteria=None):
+        """ Handles the query to get the area information dates.
+
+        Parameters
+        ----------
+        :param search_criteria:
+            Defines the search criteria used to filter the query.
+
+        Returns
+        -------
+        :return:
+            The query result in JSON format
+        """
+        if search_criteria is None:
+            search_criteria = {}
+
+        url = self.server_url + "/v1/area/information/dates?access_token=" + \
+              urllib.parse.quote(self.access_token)
+
+        url += _generate_request_search_criteria(search_criteria.items())
+        # print(url)
+        session = requests.Session()
+        response = session.get(url)
+        if response.status_code != 200:
+            # print(response.content.decode("utf-8"))
+            raise Exception(response.content.decode("utf-8"))
+        return json.loads(response.content.decode("utf-8"))
